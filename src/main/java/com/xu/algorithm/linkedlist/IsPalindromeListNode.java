a556532c4f97f27f6452ed846e8f7e9d361c9599
CharleyXu <xucharlie1993@gmail.com>
2024-01-19 16:37:48 +0800

    feat(algo): add more algo answers

--- src/main/java/com/xu/algorithm/linkedlist/IsPalindromeListNode.java
+++ src/main/java/com/xu/algorithm/linkedlist/IsPalindromeListNode.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 /**
  * Created by CharleyXu on 2023/11/29
+ * <p>
+ * 234 回文链表
  */
 public class IsPalindromeListNode {
 
@@ -11,8 +13,44 @@ public class IsPalindromeListNode {
      * 快慢指针 + 链表翻转
      * <p>
      * [1,2,3,3,2,1]
+     * <p>
+     * 时间复杂度 O(N)，空间复杂度 O(1)
      */
     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        // 快慢指针找中间节点
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        // 反转 后半部分链表
+        ListNode pre = null;
+        ListNode cur = slow.next;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            // 指针前移
+            pre = cur;
+            cur = temp;
+        }
+        // 判断是否回文
+        ListNode p1 = head;
+        ListNode p2 = pre;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
+    }
+
+    // 快慢指针 + 链表翻转
+    public boolean isPalindrome2(ListNode head) {
         if (head == null || head.next == null) {
             return true;
         }
@@ -41,7 +79,8 @@ public class IsPalindromeListNode {
 
     @Test
     public void isPalindromeTest() {
-        System.out.println(isPalindrome(head));
+//        System.out.println(isPalindrome(head));
+        System.out.println(isPalindrome2(head));
     }
 
     ListNode node5 = new ListNode(1, null);
